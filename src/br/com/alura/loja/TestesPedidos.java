760bda9d831f4eaef95654009e66ee70bb43b241
Anderson Pereira <andersononus@hotmail.com>
2021-07-14 20:03:55 -0300

    Observer

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -2,8 +2,11 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class TestesPedidos {
 
@@ -13,8 +16,10 @@ public class TestesPedidos {
         int quantidadeItens = 3;
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        GeraPedidoHandler handler = new GeraPedidoHandler();
-        handler.executar(gerador);
+        GeraPedidoHandler handler = new GeraPedidoHandler(
+                Arrays.asList(new SalvarPedidoNoBancoDeDados(),
+                        new EnviarEmailPedido()));
+        handler.execute(gerador);
     }
 
 }
--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,21 +1,24 @@
 package br.com.alura.loja.pedido;
 
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class GeraPedidoHandler {
-    //Dependency injection
-    public GeraPedidoHandler() {
+    private List<AcaoAposGerarPedido> acoes;
+
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+        this.acoes = acoes;
     }
 
-    public void executar(GeraPedido geraPedido) {
+    public void execute(GeraPedido geraPedido) {
         Orcamento orcamento = new Orcamento(geraPedido.getValorOrcamento(), geraPedido.getQuantidadeItens());
         Pedido pedido = new Pedido(geraPedido.getCliente(), LocalDateTime.now(), orcamento);
-
-        // utilizacao dos servicos de infra
-        System.out.println("Salvando pedido no banco de dados...");
-        System.out.println("Enviando email para cliente sobre pedido...");
+        acoes.forEach(a -> a.executarAcao(pedido));
     }
 
 }
