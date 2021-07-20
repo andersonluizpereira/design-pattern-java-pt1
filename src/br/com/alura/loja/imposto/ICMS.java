53ad00bffe11fd11aa201d9c7ec0bff1c2172447
Anderson Pereira <andersononus@hotmail.com>
2021-07-20 19:10:37 -0300

    decorator

--- src/br/com/alura/loja/imposto/ICMS.java
+++ src/br/com/alura/loja/imposto/ICMS.java
@@ -4,8 +4,14 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public class ICMS implements Imposto {
-    public BigDecimal calcular(Orcamento orcamento){
+public class ICMS extends Imposto {
+    public ICMS(Imposto outro) {
+        super(outro);
+    }
+
+    @Override
+    protected BigDecimal realizarCalculoEspecifico(Orcamento orcamento) {
         return orcamento.getValor().multiply(new BigDecimal("0.1"));
     }
+
 }
--- src/br/com/alura/loja/imposto/Imposto.java
+++ src/br/com/alura/loja/imposto/Imposto.java
@@ -4,6 +4,20 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public interface Imposto {
-    BigDecimal calcular(Orcamento orcamento);
+public abstract class Imposto {
+    private Imposto outro;
+
+    public Imposto(Imposto outro) {
+        this.outro = outro;
+    }
+
+    protected abstract BigDecimal realizarCalculoEspecifico(Orcamento orcamento);
+    public BigDecimal calcular(Orcamento orcamento) {
+        BigDecimal valorImposto = realizarCalculoEspecifico(orcamento);
+        BigDecimal valorOutroImposto = BigDecimal.ZERO;
+        if(outro != null) {
+            valorOutroImposto = outro.realizarCalculoEspecifico(orcamento);
+        }
+        return valorImposto.add(valorOutroImposto);
+    }
 }
